e5818090172af891e996d4a16b1498ab91d68d8e
Vipul Kumar <kumarvipul479@gmail.com>
2021-08-08 17:26:42 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:kowi_intern/Videos.dart';
+import 'package:kowi/Videos.dart';
 import 'package:video_player/video_player.dart';
 
 void main() {
